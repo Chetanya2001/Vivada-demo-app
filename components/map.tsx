e2f0252b51b8f4b3f13f9805a3ca6607f99cbc10
Chetanya2001 <mathurchetanya1@gmail.com>
2024-10-08 16:26:44 +0530

    done

--- components/map.tsx
+++ components/map.tsx
@@ -1,17 +1,81 @@
-import React, {useMemo} from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import Svg, { G, Path, Circle } from "react-native-svg";
+import React, { useState, useEffect, useMemo } from 'react';
+import { View } from 'react-native';
+import Svg, { G, Path, Circle } from 'react-native-svg';
+import * as d3 from 'd3';
 import { COUNTRIES } from '../constants/countryshapes';
-const Map = (props:any) => {
-    const { width, height } = Dimensions.get('window');
-    const mapextent = useMemo(() => {
-        return width > height / 2 ? height / 2 : width;
-    },[width,height]);
-    return (
-       <View>
-          <Text>Our Map</Text>
-       </View>
+
+interface MapProps {
+  dimensions: {
+    width: number;
+    height: number;
+  };
+}
+
+interface CountryFeature extends GeoJSON.Feature<GeoJSON.Geometry> {
+  properties: {
+    name: string;
+  };
+}
+
+const Map: React.FC<MapProps> = ({ dimensions }) => {
+  const [countryList, setCountryList] = useState<JSX.Element[]>([]);
+
+  const mapExtent = useMemo(() => {
+    return dimensions.width > dimensions.height / 2
+      ? dimensions.height / 2
+      : dimensions.width;
+  }, [dimensions]);
+
+  const countryPaths = useMemo(() => {
+    const projection = d3
+      .geoAzimuthalEqualArea()
+      .rotate([0, -90])
+      .clipAngle(150)
+      .fitSize([mapExtent, mapExtent], {
+        type: 'FeatureCollection',
+        features: COUNTRIES,
+      })
+      .translate([dimensions.width / 2, mapExtent / 2]);
+
+    const geoPath = d3.geoPath().projection(projection);
+
+    const svgPaths = COUNTRIES.map((country: CountryFeature) => geoPath(country));
+    return svgPaths;
+  }, [dimensions]);
+
+  useEffect(() => {
+    setCountryList(
+      countryPaths.map((path: string | null | undefined, i: number) => {
+        return (
+          <Path
+            key={COUNTRIES[i].properties.name}
+            d={path || ''}
+            stroke={'#aaa'}
+            strokeOpacity={0.3}
+            strokeWidth={0.6}
+            fill={'#aaa'}
+            opacity={0.4}
+          />
+        );
+      })
     );
- };
- const styles = StyleSheet.create({});
- export default Map;
+  }, [countryPaths]);
+
+  return (
+    <View>
+      <Svg width={dimensions.width} height={dimensions.height / 2}>
+        <G>
+          <Circle
+            cx={dimensions.width / 2}
+            cy={mapExtent / 2}
+            r={mapExtent / 2}
+            fill={'#3b454f'}
+          />
+          {countryList.map((country) => country)}
+        </G>
+      </Svg>
+    </View>
+  );
+};
+
+export default Map;
